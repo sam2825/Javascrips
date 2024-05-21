2bd0751ac1ebb8f46f38bcc09af3839bbab74ae7
sam2825 <mssarita23@gmail.com>
2024-05-21 10:06:50 -0500

    ajustes al juego 2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function jugar(eleccionUsuario) {
   let eleccionMaquina = opciones[Math.floor(Math.random() * opciones.length)];
   console.log("Tu eliges: " + eleccionUsuario);
   console.log("La máquina elige: " + eleccionMaquina)
-  
+
   eleccionUsuario = eleccionUsuario.charAt(0).toUpperCase() + eleccionUsuario.slice(1);
   if (
     (eleccionUsuario === "Piedra" && eleccionMaquina === "Tijeras") ||
@@ -21,16 +21,20 @@ function jugar(eleccionUsuario) {
     console.log("Empate.");
   }
 }
-while (true) {
-  let eleccionUsuario = prompt("Elige Piedra, Papel o Tijeras (o escribe 'salir' para terminar):").toLowerCase();
-  if (eleccionUsuario === "salir") {
-    console.log("Gracias por jugar. ¡Hasta luego!");
-    break;
-  }
- if (eleccionUsuario !== "piedra" && eleccionUsuario !== "papel" && eleccionUsuario !== "tijeras") {
-    console.log("Error: Opción no válida. Por favor elige Piedra, Papel o Tijeras.");
-    continue;
-  }
+if (confirm("¿Estás listo para jugar?")) {
+  while (true) {
+    let eleccionUsuario = prompt("Elige Piedra, Papel o Tijeras (o escribe 'salir' para terminar):").toLowerCase();
+    if (eleccionUsuario === "salir") {
+      console.log("Gracias por jugar. ¡Hasta luego!");
+      break;
+    }
+    if (eleccionUsuario !== "piedra" && eleccionUsuario !== "papel" && eleccionUsuario !== "tijeras") {
+      console.log("Error: Opción no válida. Por favor elige Piedra, Papel o Tijeras.");
+      continue;
+    }
 
-  jugar(eleccionUsuario);
+    jugar(eleccionUsuario);
+  }
+} else {
+  console.log("Quizás en otra ocasión. ¡Hasta luego!");
 }
